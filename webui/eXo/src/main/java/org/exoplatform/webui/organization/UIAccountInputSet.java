bb6decbefd3e78c40d4688e3c0f87a784d4b1e2a
Nguyen Dang Tung <tung.dang@exoplatform.com>
2008-01-30 03:44:13 +0000

    change interface UIAccountForm

--- webui/eXo/src/main/java/org/exoplatform/webui/organization/UIAccountInputSet.java
+++ webui/eXo/src/main/java/org/exoplatform/webui/organization/UIAccountInputSet.java
@@ -19,10 +19,9 @@ package org.exoplatform.webui.organization;
 import org.exoplatform.services.organization.OrganizationService;
 import org.exoplatform.services.organization.User;
 import org.exoplatform.web.application.ApplicationMessage;
-import org.exoplatform.webui.Util;
 import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.core.UIApplication;
-import org.exoplatform.webui.form.UIFormInputSet;
+import org.exoplatform.webui.form.UIFormInputWithActions;
 import org.exoplatform.webui.form.UIFormStringInput;
 import org.exoplatform.webui.form.validator.EmailAddressValidator;
 import org.exoplatform.webui.form.validator.EmptyFieldValidator;
@@ -36,7 +35,7 @@ import org.exoplatform.webui.form.validator.StringLengthValidator;
  *          minhdv81@yahoo.com
  * Jun 28, 2006
  */
-public class UIAccountInputSet extends UIFormInputSet {
+public class UIAccountInputSet extends UIFormInputWithActions {
   
   final static String USERNAME = "username" ;
   final static String PASSWORD1X = "password" ;
@@ -44,6 +43,7 @@ public class UIAccountInputSet extends UIFormInputSet {
   
   public UIAccountInputSet(String name) throws Exception {
     super(name);
+    //setComponentConfig(getClass(), null) ;
     addUIFormInput(new UIFormStringInput(USERNAME, "userName", null).
                    addValidator(EmptyFieldValidator.class).
                    addValidator(IdentifierValidator.class).
