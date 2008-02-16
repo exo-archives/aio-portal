7353ba587ec20719a86344c7de429c05060d3c34
Pham Thanh Tung <tung.pham@exoplatform.com>
2008-02-16 10:39:07 +0000

    Config template for Home Page in [web/portal] package

---
+++ web/portal/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -0,0 +1,21 @@
+/***************************HomePagePortlet**************************************************/
+SkinService.addSkin(
+    "web/HomePagePortlet",
+    "Default",
+    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "web/HomePagePortlet",
+    "Vista",
+    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+ 
+SkinService.addSkin(
+    "web/HomePagePortlet",
+    "Mac",
+    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
