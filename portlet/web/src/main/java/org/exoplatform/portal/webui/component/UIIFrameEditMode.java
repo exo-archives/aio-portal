5d325df3ec0643a5f6f1fb945862019ecb8a4753
Tan Pham Dinh <tan.pham@exoplatform.com>
2008-05-23 02:30:51 +0000

    fix bug portal-1540

--- portlet/web/src/main/java/org/exoplatform/portal/webui/component/UIIFrameEditMode.java
+++ portlet/web/src/main/java/org/exoplatform/portal/webui/component/UIIFrameEditMode.java
@@ -32,7 +32,6 @@ import org.exoplatform.webui.event.EventListener;
 import org.exoplatform.webui.exception.MessageException;
 import org.exoplatform.webui.form.UIForm;
 import org.exoplatform.webui.form.UIFormStringInput;
-import org.exoplatform.webui.form.validator.NullFieldValidator;
 
 /**
  * Created by The eXo Platform SARL Author : Tran The Trong trongtt@gmail.com
@@ -48,12 +47,21 @@ public class UIIFrameEditMode extends UIForm {
         .getCurrentInstance();
     PortletPreferences pref = pcontext.getRequest().getPreferences();
     addUIFormInput(new UIFormStringInput(FIELD_URL, FIELD_URL, pref.getValue("url",
-        "http://www.exoplatform.com")).addValidator(NullFieldValidator.class));
+        "http://www.exoplatform.com")));
   }
 
   static public class SaveActionListener extends EventListener<UIIFrameEditMode> {
     public void execute(Event<UIIFrameEditMode> event) throws Exception {
-      String url = event.getSource().getUIStringInput(FIELD_URL).getValue();
+      UIIFrameEditMode uiForm = event.getSource();
+      String url = uiForm.getUIStringInput(FIELD_URL).getValue();
+      if(url==null || url.length()==0){
+        UIIFramePortlet uiPortlet = uiForm.getParent();
+        
+        Object args[] = {uiForm.getLabel(uiForm.getUIStringInput(FIELD_URL).getId())};
+        uiPortlet.addMessage(new ApplicationMessage("EmptyFieldValidator.msg.empty-input", args));
+        uiForm.getUIStringInput(FIELD_URL).setValue(uiPortlet.getURL());
+        return;
+      }
       try {
         new URL(url);
         PortletRequestContext pcontext = (PortletRequestContext) WebuiRequestContext
