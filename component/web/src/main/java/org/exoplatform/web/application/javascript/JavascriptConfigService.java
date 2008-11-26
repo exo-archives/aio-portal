4bc638e30181548bdae81adda50277bc0dff4367
Trong The Tran <trong.tran@exoplatform.com>
2008-11-26 11:22:22 +0000

    a patch to fix PORTAL-2329 for 2.1.2

---
+++ component/web/src/main/java/org/exoplatform/web/application/javascript/JavascriptConfigService.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2003-2007 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.web.application.javascript;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.servlet.ServletContext;
+
+public class JavascriptConfigService {
+
+	private Collection<String> availableScripts_;
+	private Collection<String> availableScriptsPaths_;
+
+	private String mergedJavascript = "";
+
+	private ByteArrayOutputStream jsStream_ = null;
+
+	public JavascriptConfigService() {
+		availableScripts_ = new ArrayList<String>();
+		availableScriptsPaths_ = new ArrayList<String>();
+	}
+
+	/**
+	 * return a collection  list This method should return the
+	 * availables scripts in the service
+	 * 
+	 * @return
+	 */
+	public Collection<String> getAvailableScripts() {
+		return availableScripts_;
+	}
+
+	public Collection<String> getAvailableScriptsPaths() {
+		return availableScriptsPaths_;
+	}  
+	
+	/**
+	 * This method allow use to add javascript at runtime
+	 * */
+	public void addJavascript(String module, String scriptPath,ServletContext scontext,String javascript) {
+    availableScripts_.add(module);        
+    availableScriptsPaths_.add("/" + scontext.getServletContextName() + scriptPath);
+    mergedJavascript = mergedJavascript.concat(javascript).concat("\n");
+    jsStream_ = null ;
+  }
+	
+	/**
+	 * 
+	 * @param module
+	 * @param skinName
+	 * @param cssPath
+	 */
+	public void addJavascript(String module, String scriptPath, ServletContext scontext) {
+		String servletContextName = scontext.getServletContextName();		
+		availableScripts_.add(module);		
+		availableScriptsPaths_.add("/" + servletContextName + scriptPath);
+		String javascript = loadJavascript(scriptPath, scontext) ;
+		mergedJavascript = mergedJavascript.concat(javascript);		
+	}
+	
+	/**
+   * This method allow remove a javascript module.
+   * If use this method, all added javascript at runtime will be lost because 
+   * this method can not load from db or other storages
+   * */
+	public void removeJavaScript(String module,String scriptPath, ServletContext scontext) {				
+		String servletContextName = scontext.getServletContextName();						
+		availableScripts_.remove(module);
+		availableScriptsPaths_.remove("/" + servletContextName + scriptPath);		
+		StringBuilder sB = new StringBuilder();
+		for(String key:availableScriptsPaths_) {
+		  int index = key.indexOf("/", 1) ;		  
+			String contextName = key.substring(0, index);
+			String jsPath  = key.substring(index) ;						
+			try {
+				ServletContext servletContext = scontext.getContext(contextName) ;				
+				String javascript = loadJavascript(jsPath, servletContext) ;				
+				sB.append(javascript);				
+			} catch (Exception e) {			  
+			}			
+		}
+		mergedJavascript = sB.toString();
+		jsStream_ = null ;
+	}	
+
+	public byte[] getMergedJavascript() {
+		if(jsStream_ == null) {
+			jsStream_ = new ByteArrayOutputStream();
+			ByteArrayInputStream input = new ByteArrayInputStream(mergedJavascript.getBytes());
+			JSMin jsMin = new JSMin(input,jsStream_);
+			try {
+				jsMin.jsmin();				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return jsStream_.toByteArray();
+	}
+
+	public boolean isModuleLoaded(CharSequence module) {
+		return getAvailableScripts().contains(module);
+	}
+	
+	private String loadJavascript(String scriptPath,ServletContext scontext) {
+    StringBuffer sB = new StringBuffer();
+    String line = ""; 
+    try {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(scontext
+          .getResourceAsStream(scriptPath)));
+      try {
+        while ((line = reader.readLine()) != null) {
+          sB.append(line + "\n");
+        }
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      } finally {
+        try {
+          reader.close();
+        } catch (Exception ex) {
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    sB.append("\n");
+    return sB.toString();
+  }
+}
