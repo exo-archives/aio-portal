a0e654f6ee55ce40c2f421a4c93b924ac8109f27
Trong The Tran <trong.tran@exoplatform.com>
2009-02-02 07:47:17 +0000

    PORTAL-2608 Browser windows resize in IE looses the divs with relative or absolute position

--- web/eXoResources/src/main/webapp/javascript/eXo/core/UIMaskLayer.js
+++ web/eXoResources/src/main/webapp/javascript/eXo/core/UIMaskLayer.js
@@ -22,9 +22,7 @@ UIMaskLayer.prototype.createTransparentMask = function() {
 		document.getElementsByTagName("body")[0].appendChild(mask);
 	}
 	mask.style.width = "100%" ;
-	mask.style.height = eXo.core.Browser.getBrowserHeight() + "px" ;
-	mask.resizeId = eXo.core.DOMUtil.generateId("script_transparentMask_resize_") ;
-	eXo.core.Browser.addOnResizeCallback(mask.resizeId, eXo.core.UIMaskLayer.resizeTransparentMask) ;
+	mask.style.height = "100%" ;
 };
 /**
  * Hides the transparent mask
@@ -35,7 +33,6 @@ UIMaskLayer.prototype.removeTransparentMask = function() {
 	if (mask) {
 		mask.style.height = "0" ;
 		mask.style.width = "0" ;
-		eXo.core.Browser.onResizeCallback.remove(mask.getAttribute("resizeId")) ;
 	}
 };
 /**
@@ -85,7 +82,7 @@ UIMaskLayer.prototype.createMask = function(blockContainerId, object, opacity, p
 		maskLayer.id = "MaskLayer" ;
 		maskLayer.maxZIndex = 3 ;
 		maskLayer.style.width = "100%"  ;
-		maskLayer.style.height = eXo.core.Browser.getBrowserHeight() + "px" ;
+		maskLayer.style.height = "100%" ;
 		maskLayer.style.top = "1px" ;
 		maskLayer.style.left = "1px" ;
 		maskLayer.style.zIndex = maskLayer.maxZIndex ;
@@ -118,8 +115,6 @@ UIMaskLayer.prototype.createMask = function(blockContainerId, object, opacity, p
 		if(maskLayer.parentNode.id == "UIPage") {
 			eXo.core.UIMaskLayer.enablePageDesktop(false);
 	  }
-		maskLayer.resizeId = eXo.core.DOMUtil.generateId("script_masklayer_resize_") ;
-		eXo.core.Browser.addOnResizeCallback(maskLayer.resizeId, eXo.core.UIMaskLayer.resizeMaskLayer) ;
 	}catch(err) {
 		alert(err) ;
 	}
@@ -233,7 +228,6 @@ UIMaskLayer.prototype.removeMask = function(maskLayer) {
 	  }
 
   	parentNode.removeChild(maskLayer) ;
-		eXo.core.Browser.onResizeCallback.remove(maskLayer.getAttribute("resizeId")) ;
 	}
 } ;
 
@@ -252,20 +246,9 @@ UIMaskLayer.prototype.enablePageDesktop = function(enabled) {
 	}
 };
 
-UIMaskLayer.prototype.resizeTransparentMask = function() {
-	var mask = document.getElementById("TransparentMaskLayer");
-	if(mask && mask.style.display != "none") {
-		mask.style.height = eXo.core.Browser.getBrowserHeight() + "px" ;
-	}
-};
 
 UIMaskLayer.prototype.resizeMaskLayer = function() {
-	var portalApp = document.getElementById("UIPortalApplication") ;
-	var masks = eXo.core.DOMUtil.findDescendantsByClass(portalApp, "div", "MaskLayer");
-	for(var i = 0; i < masks.length; i++) {
-		if(masks[i] && masks[i].style.display == "none") continue;
-		masks[i].style.height = eXo.core.Browser.getBrowserHeight() + "px" ;
-	}
+	return ;
 } ;
 
 eXo.core.UIMaskLayer = new UIMaskLayer() ;
