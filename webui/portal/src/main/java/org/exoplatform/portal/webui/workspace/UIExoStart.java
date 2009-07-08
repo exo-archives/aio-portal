ddf7bf27f7e7be650156eba4fe37f1633943517f
Nguyen Dinh Tam <tam.nguyen@exoplatform.com>
2009-07-08 07:55:20 +0000

    PORTAL-2970 Should make eXo start menu using ajax by configuration not by code

--- webui/portal/src/main/java/org/exoplatform/portal/webui/workspace/UIExoStart.java
+++ webui/portal/src/main/java/org/exoplatform/portal/webui/workspace/UIExoStart.java
@@ -19,20 +19,16 @@ package org.exoplatform.portal.webui.workspace;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.portlet.WindowState;
-
-import org.exoplatform.portal.config.UserACL;
 import org.exoplatform.container.ExoContainer;
 import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.portal.account.UIAccountSetting;
 import org.exoplatform.portal.application.PortalRequestContext;
+import org.exoplatform.portal.config.UserACL;
 import org.exoplatform.portal.config.model.PageNavigation;
 import org.exoplatform.portal.webui.UIWelcomeComponent;
 import org.exoplatform.portal.webui.UIManagement.ManagementMode;
-import org.exoplatform.portal.webui.application.UIPortlet;
 import org.exoplatform.portal.webui.navigation.PageNavigationUtils;
 import org.exoplatform.portal.webui.navigation.UIPageManagement;
-import org.exoplatform.portal.webui.page.UIPageBody;
 import org.exoplatform.portal.webui.page.UIPageCreationWizard;
 import org.exoplatform.portal.webui.page.UIPageEditWizard;
 import org.exoplatform.portal.webui.page.UIWizardPageCreationBar;
@@ -96,6 +92,11 @@ public class UIExoStart extends UIComponent {
 
   public List<List<MenuItemContainer>>  getMenus() {  return menus ; }
 
+  public boolean isUseAjax() {
+    ExoContainer exoContainer = ExoContainerContext.getCurrentContainer() ;
+    UserACL acl = (UserACL) exoContainer.getComponentInstanceOfType(UserACL.class) ;
+    return acl.isEXoStartAjax();
+  }
   public List<PageNavigation> getNavigations() throws Exception {
     List<PageNavigation> allNav =Util.getUIPortal().getNavigations() ;
     String removeUser = Util.getPortalRequestContext().getRemoteUser();
@@ -139,49 +140,39 @@ public class UIExoStart extends UIComponent {
   }
 
   static public class MenuItemAction extends MenuItem {
-    private String  action;
-
+    private String action ;
     private boolean useAjax = true;
 
-    private boolean isUseAjax(){
-      ExoContainer exoContainer = ExoContainerContext.getCurrentContainer() ;
-      UserACL acl = (UserACL) exoContainer.getComponentInstanceOfType(UserACL.class) ;
-      return acl.isEXoStartAjax();
-    }
-    public MenuItemAction(String name) {
-      super(name, name + "Icon");
+    public  MenuItemAction(String name) {
+      super(name, name + "Icon") ;
       this.action = name;
     }
 
-    public MenuItemAction(String name, boolean useAjax) {
-      super(name, name + "Icon");
-      this.useAjax = isUseAjax();
+    public  MenuItemAction(String name, boolean useAjax) {
+      super(name, name + "Icon") ;
+      this.useAjax = useAjax;
       this.action = name;
     }
 
-    public MenuItemAction(String name, String icon) {
-      super(name, icon);
+    public  MenuItemAction(String name, String icon) {
+      super(name, icon) ;
       this.action = name;
     }
 
-    public MenuItemAction(String name, String icon, String action) {
-      super(name, icon);
-      this.action = action;
+    public  MenuItemAction(String name, String icon, String action) {
+      super(name, icon) ;
+      this.action = action ;
     }
 
-    public MenuItemAction(String name, String icon, String action, boolean useAjax) {
-      super(name, icon);
-      this.action = action;
-      this.useAjax = isUseAjax();
+    public  MenuItemAction(String name, String icon, String action, boolean useAjax) {
+      super(name, icon) ;
+      this.action = action ;
+      this.useAjax = useAjax ;
     }
 
-    public String getAction() {
-      return action;
-    }
+    public String getAction() { return action ; }
 
-    public boolean useAjax() {
-      return useAjax;
-    }
+    public  boolean useAjax() {  return useAjax ; }
   }
 
   public <T extends UIComponent> void setUIControlWSWorkingComponent(Class<T> clazz) throws Exception {
