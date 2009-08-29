1f82600e8ff0c1a2f82f64084323985a7b336a18
Hoang To <hoangtm@exoplatform.com>
2009-08-29 02:31:49 +0000

    Update javascript code for Tabbed Dashboard

--- web/eXoResources/src/main/webapp/javascript/eXo/webui/UITabbedDashboard.js
+++ web/eXoResources/src/main/webapp/javascript/eXo/webui/UITabbedDashboard.js
@@ -2,25 +2,30 @@ eXo.webui.UITabbedDashboard = {
 	
 	init : function(){},
 	
-	renameTabLabel : function(inputElement, keyEvent, backupContent) {
-		var keynum = keyEvent.keyCode;
+	renameTabLabel : function(e){
+		if(!e){
+			e = windwow.event;
+		}
+		var keyNum = e.keyCode;
 		
-		//if user presses on ENTER key, then rename the tab
-		if(keynum == 13){
+		//If user presses on ENTER button, then rename the btton
+		if(keyNum == 13){
+			var inputElement = eXo.core.Browser.getEventSource(e);
 			var newTabLabel = inputElement.value;
 			if(newTabLabel.length < 1){
 				return;
 			}
 			var DOMUtil = eXo.core.DOMUtil;
-			var parent = DOMUtil.findAncestorByClass(inputElement, "MiddleTab");
-			var newTabContent = "<span ondblclick='eXo.webui.UITabbedDashboard.showEditLabelInput(this)'>" +
-		                	newTabLabel + "</span>";
-			parent.innerHTML = newTabContent;
-			
-			//Send request to server to change node name
-			var portletFrag = DOMUtil.findAncestorByClass(parent, "PORTLET-FRAGMENT");
+			var portletFrag = DOMUtil.findAncestorByClass(inputElement, "PORTLET-FRAGMENT");
 			var compId = portletFrag.parentNode.id;
 			var nodeIndex = inputElement.id;
+			
+			//Change the tab label
+			var spanElement = document.createElement("span");
+			spanElement.innerHTML = newTabLabel;
+			inputElement.parentNode.replaceChild(spanElement, inputElement);
+			
+			//Send request to server to change node name
 			var href = eXo.env.server.portalBaseURL + "?portal:componentId=" + compId;
 			href += "&portal:type=action";
 			href += "&portal:isSecure=false";
@@ -29,25 +34,27 @@ eXo.webui.UITabbedDashboard = {
 			href += "&objectId=" + nodeIndex;
 			href += "&newTabLabel=" + newTabLabel;
 			window.location = href;
-			return;
+			return;			
 		}
-		//If user presses on ESCAPE key, then reset ancient label
-		else if(keynum == 27){
-			if(backupContent.length < 1){
-				return;
-			}
-			//window.location = eXo.env.server.portalBaseURL;
-			}	
+		//If user presses on the ESCAPE key, then reset the span element on the tab
+		else if(keyNum == 27){
+			var inputElement = eXo.core.Browser.getEventSource(e);
+			var spanElement = document.createElement("span");
+			spanElement.ondblclick = "#"; //TODO: Set the callback on this property
+			spanElement.innerHTML = inputElement.name;
+			inputElement.parentNode.replaceChild(spanElement, inputElement);	
+		}
 	},
-	
+
 	showEditLabelInput : function(selectedElement, nodeIndex, currentContent){
-		var inputElement = "<input type='text' ";
-		inputElement += " id='" + nodeIndex + "'";
-		inputElement += " value='" + currentContent +"'";
-		inputElement += " style='border:medium none;'"
-		inputElement += " onkeypress='" + "eXo.webui.UITabbedDashboard.renameTabLabel(this, event,";
-		inputElement += "\"" + currentContent + "\")' />";
-		selectedElement.parentNode.innerHTML = inputElement;
-		
+
+		var inputElement = document.createElement("input");
+		inputElement.type = "text";
+		inputElement.id = nodeIndex;
+		inputElement.name = currentContent; // To store old value
+		inputElement.value = currentContent;
+		inputElement.style.border = "medium none";
+		inputElement.onkeypress = eXo.webui.UITabbedDashboard.renameTabLabel;
+		selectedElement.parentNode.replaceChild(inputElement, selectedElement);
 	}
 }
