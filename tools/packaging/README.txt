20601c7908fc51adb31339df6bd14c36b324dffc
Dimitri Baeli <dimitri.baeli@exoplatform.com>
2009-09-01 03:43:52 +0000

    EXB-69: exobuild renamed exopackage * embedded in integration packaging * new documentation * tomcat and jboss packaging support (preview)

--- tools/packaging/README.txt
+++ tools/packaging/README.txt
@@ -1,8 +1,40 @@
+Portal packaging tooling:
+* This pom embbed and autorun and exobuild
+* http://svn.exoplatform.org/projects/utils/exopackage/tags/1.0.0
+* http://svn.exoplatform.org/projects/utils/exopackage-conf/tags/20090901 
 
-Work in progress with the simplification of exobuild
+Process:
+1/ The exo-tomcat or jboss packaging is done during the "mvn install" process of the "packaging" module
+2/ exopackage-1.0.0 is downloaded from maven
+2/ exopackage-conf-20090901 is downloaded from maven, and contains official modules for exoproducts
+4/ product and module for this version are stored in the current project, and find by exopackage during the run
+5/ a maven-exec-plugin calls exobuild from maven with the right parameters
+6/ the delivery is in the target directory
 
-In order to avoid exobuild to fetch binaries in maven repositories by himself, this pom.xml declares the missing dependencies and mvn dependency:tree will ask maven to do the job
-(M2_REMOTE_REPOS will only have to contain the local repo in exobuild conf)
-1/ In tools/packaging dir:
-** mvn dependency:tree (or dependency:go-offline)
-** exobuild --product=portal --verions=integration --deploy=tomcat
+
+*****************
+* CONFIGURATION:
+*****************
+
+* in your settings.xml file (maven local computer configuration) you should at least add this
+    <profile>
+      <id>exo-projects</id>
+      <activation><activeByDefault>false</activeByDefault></activation>
+      <properties>
+        <exo.projects.directory.dependencies>c:/ExoPlatform/src/exo-dependencies</exo.projects.directory.dependencies> 
+        <exo.projects.app.tomcat.version>tomcat-6.0.16</exo.projects.app.tomcat.version>
+        <exo.projects.app.jboss.version>jboss-4.2.0.GA</exo.projects.app.jboss.version> 
+      </properties>
+    </profile>
+* exo.projects.directory.dependencies should contain :
+** ${exo.projects.app.tomcat.version}/ a Tomcat clean installation, to be used as packaging template
+** ${exo.projects.app.jboss.version}/ a JBoss clean installation, to be used as packaging template
+
+*****************
+* PACKAGING:
+*****************
+
+* mvn install
+** Create a Tomcat delivery in target\working\exo-tomcat\
+* mvn install -Pjboss
+** Create a Tomcat delivery in target\working\exo-jboss\
---
+++ tools/packaging/products/portal/integration.js
@@ -0,0 +1,51 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "eXoPortal" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "portal" ;//module in modules/portal/module.js
+  product.serverPluginVersion = "2.6-int-SNAPSHOT"
+
+  var kernel = Module.GetModule("kernel/tags/2.2.Alpha3") ;
+  var core = Module.GetModule("core/tags/2.3.Alpha4") ;
+  var ws = Module.GetModule("ws/tags/2.1.Alpha4", {kernel : kernel, core : core});
+  var eXoJcr = Module.GetModule("jcr/tags/1.12.Alpha4", {kernel : kernel, core : core, ws : ws}) ;
+  
+//  var kernel = Module.GetModule("kernel/tags/2.1.2") ;
+//  var core = Module.GetModule("core/tags/2.2.2") ;
+//  var ws = Module.GetModule("ws/tags/2.0.2", {kernel : kernel, core : core});
+//  var eXoJcr = Module.GetModule("jcr/tags/1.11.2", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal", {kernel : kernel, ws:ws, core : core, eXoJcr : eXoJcr});
+  
+  product.addDependencies(portal.web.rest) ;
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.portlet.dashboard) ;
+  product.addDependencies(portal.eXoGadgetServer) ;
+  product.addDependencies(portal.eXoGadgets) ;
+  product.addDependencies(portal.webui.portal);
+  
+  product.addDependencies(portal.web.eXoResources);
+  product.addDependencies(portal.web.eXoMacSkin);
+  product.addDependencies(portal.web.eXoVistaSkin);
+
+  product.addDependencies(portal.web.portal) ;
+
+  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jbossear",  portal.server.jbossear.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+  product.addServerPatch("ear",  portal.server.websphere.patch) ;
+
+  /* cleanup duplicated lib */
+  product.removeDependency(new Project("commons-httpclient", "commons-httpclient", "jar", "3.0"));
+  product.removeDependency(new Project("commons-collections", "commons-collections", "jar", "3.1"));
+
+  product.module = portal ;
+  product.dependencyModule = [ kernel, core, ws, eXoJcr];
+
+  return product ;
+}
