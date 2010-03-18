8b6bdfde429d28ce590e8058af670b397ca9dd13
Hoang To <hoangtm@exoplatform.com>
2010-03-18 04:14:08 +0000

    GTNPORTAL-3704: Protocol is hardcoded in Gadgets.js

--- web/eXoResources/src/main/webapp/javascript/eXo/gadget/Gadgets.js
+++ web/eXoResources/src/main/webapp/javascript/eXo/gadget/Gadgets.js
@@ -791,7 +791,7 @@ gadgets.IfrGadget.prototype.setNoCache = function(value) {
  */
 gadgets.Container = function() {
   this.gadgets_ = {};
-  this.parentUrl_ = 'http://' + document.location.host;
+  this.parentUrl_ = window.location.protocol + '//' + document.location.host;
   this.country_ = 'ALL';
   this.language_ = 'ALL';
   this.view_ = 'default';
